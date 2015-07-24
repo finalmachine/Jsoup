1c7568fb676a36af9390781bb0429809f6477db1
leo.li <leo.li@generalbiologic.com>
2015-07-24 14:39:21 +0800

    finish test6

--- src/main/java/com/gbi/jsoup/query/CssQueryTest.java
+++ src/main/java/com/gbi/jsoup/query/CssQueryTest.java
@@ -1,19 +1,18 @@
 package com.gbi.jsoup.query;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import com.gbi.jsoup.util.ResourceUtil;
+import com.gbi.commons.util.file.ResourceUtil;
 
 public class CssQueryTest {
 
-	public static void test1() throws IOException {
+	public static void test1() throws Exception {
 		System.out.println("------test1----->\n");
-		File file = ResourceUtil.getSelfFile(CssQueryTest.class, "test1.html");
+		File file = ResourceUtil.getTestFile(CssQueryTest.class, "test1.html");
 		Document dom = Jsoup.parse(file, "UTF-8");
 		System.out.println("div:" + dom.select("div").size());
 		System.out.println("div>table:" + dom.select("div>table").size());
@@ -25,9 +24,9 @@ public class CssQueryTest {
 		System.out.println("\n<-----test1------");
 	}
 	
-	public static void test2() throws IOException {
+	public static void test2() throws Exception {
 		System.out.println("------test2----->\n");
-		File file = ResourceUtil.getSelfFile(CssQueryTest.class, "test2.html");
+		File file = ResourceUtil.getTestFile(CssQueryTest.class, "test2.html");
 		Document dom = Jsoup.parse(file, "UTF-8");
 		System.out.println("div:eq(0):");
 		for (Element e : dom.select("div:eq(0)")) {
@@ -48,18 +47,18 @@ public class CssQueryTest {
 		System.out.println("\n<-----test2------");
 	}
 	
-	public static void test3() throws IOException {
+	public static void test3() throws Exception {
 		System.out.println("------test3----->\n");
-		File file = ResourceUtil.getSelfFile(CssQueryTest.class, "test3.html");
+		File file = ResourceUtil.getTestFile(CssQueryTest.class, "test3.html");
 		Document dom = Jsoup.parse(file, "UTF-8");
 		System.out.println("size of 'body div':" + dom.select("body div").size());
 		System.out.println("size of 'body>div':" + dom.select("body>div").size());
 		System.out.println("\n<-----test3------");
 	}
 	
-	public static void test4() throws IOException {
+	public static void test4() throws Exception {
 		System.out.println("------test4----->\n");
-		File file = ResourceUtil.getSelfFile(CssQueryTest.class, "test4.html");
+		File file = ResourceUtil.getTestFile(CssQueryTest.class, "test4.html");
 		Document dom = Jsoup.parse(file, "UTF-8");
 		System.out.println("div>div+table:" + dom.select("div>div+table").first().id());
 		System.out.println("div>div+table+span:" + dom.select("div>div+table+span").first().id());
@@ -68,9 +67,9 @@ public class CssQueryTest {
 		System.out.println("\n<-----test4------");
 	}
 	
-	public static void test5() throws IOException {
+	public static void test5() throws Exception {
 		System.out.println("------test5----->\n");
-		File file = ResourceUtil.getSelfFile(CssQueryTest.class, "test5.html");
+		File file = ResourceUtil.getTestFile(CssQueryTest.class, "test5.html");
 		Document dom = Jsoup.parse(file, "UTF-8");
 		Element element = dom.select("meta[http-equiv=Content-Type][content*=charset]").first();
 		System.out.println(element == null);
@@ -78,16 +77,21 @@ public class CssQueryTest {
 		System.out.println("\n<-----test5------");
 	}
 	
-	public static void test6() throws IOException {
-		
+	public static void test6() throws Exception {
+		System.out.println("------test6----->\n");
+		File file = ResourceUtil.getTestFile(CssQueryTest.class, "test6.html");
+		Document dom = Jsoup.parse(file, "UTF-8");
+		System.out.println(dom.select("span.sums>strong:containsOwn(医院地址)").first().nextElementSibling());
+		System.out.println(dom.select("span.sums>strong:containsOwn(医院电话)").first().nextSibling());
+		System.out.println("\n<-----test6------");
 	}
-	
-	public static void main(String[] args) throws IOException {
-		test1();
-		test2();
-		test3();
-		test4();
-		test5();
+
+	public static void main(String[] args) throws Exception {
+	//	test1();
+	//	test2();
+	//	test3();
+	//	test4();
+	//	test5();
 		test6();
 	}
 }
